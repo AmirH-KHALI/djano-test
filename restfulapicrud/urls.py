1a444f66cc0bd6d8b951b119d79b095882abf257
AmirH Ahmadi <amirh.khali@gmail.com>
2020-10-20 07:36:10 +0330

    add all

--- restfulapicrud/urls.py
+++ restfulapicrud/urls.py
@@ -1,7 +1,7 @@
-"""test1 URL Configuration
+"""restfulapicrud URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from .router import router
+from django.conf.urls import url
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='Swagger Test!')
 
 urlpatterns = [
+    path('api_documentation/', schema_view),
     path('admin/', admin.site.urls),
+    path('api/',include(router.urls)),
 ]
